2d639854fd39663b9175b84812a72f9cc4668bea
liflow <124431733+liflow@users.noreply.github.com>
2023-02-24 18:42:57 +0900

    Add files via upload
    data extraction 편한 ver.

--- converter.cpp
+++ converter.cpp
@@ -1,4 +1,5 @@
 #pragma comment(lib, "k4a.lib")
+#define _CRT_SECURE_NO_WARNINGS
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
@@ -22,7 +23,7 @@ int main()
 {
     //MKV 파일 열기
     k4a_playback_t playback_handle;
-    if (k4a_playback_open("output.mkv", &playback_handle) != K4A_RESULT_SUCCEEDED)
+    if (k4a_playback_open("m1_5.mkv", &playback_handle) != K4A_RESULT_SUCCEEDED)
     {
         printf("Failed to open recording\n");
         return 1;
@@ -76,9 +77,12 @@ int main()
     k4a_playback_set_color_conversion(playback_handle, K4A_IMAGE_FORMAT_IR16);
     k4a_playback_set_color_conversion(playback_handle, K4A_IMAGE_FORMAT_COLOR_YUY2);
 
+    FILE* fp = fopen("m1_5.txt", "w");
+
     k4a_capture_t capture;
-    k4a_stream_result_t stream_result = k4a_playback_get_next_capture(playback_handle, &capture);
-    while (stream_result == K4A_STREAM_RESULT_SUCCEEDED)
+    k4a_stream_result_t stream_result;
+
+    while ((stream_result = k4a_playback_get_next_capture(playback_handle, &capture)) == K4A_STREAM_RESULT_SUCCEEDED)
     {
         // Body Tracking 실행
         k4a_wait_result_t queue_capture_result = k4abt_tracker_enqueue_capture(tracker, capture, K4A_WAIT_INFINITE);
@@ -93,22 +97,31 @@ int main()
         if (pop_frame_result == K4A_WAIT_RESULT_SUCCEEDED)
         {
             size_t num_bodies = k4abt_frame_get_num_bodies(body_frame);
-            printf("Detected bodies: %zu\n", num_bodies);
+            /*printf("Detected bodies:%zu\n", num_bodies);
+            fprintf(fp, "Detected bodies:%zu\n", num_bodies);*/
             // 사람의 좌표 정보를 출력합니다.
             for (size_t i = 0; i < num_bodies; i++)
             {
                 k4abt_skeleton_t skeleton;
                 k4a_result_t result = k4abt_frame_get_body_skeleton(body_frame, i, &skeleton);
+
                 if (result == K4A_RESULT_SUCCEEDED)
                 {
-                    printf("Body ID: %u\n", k4abt_frame_get_body_id(body_frame, i));
+                    /*printf("Body ID: %u\n", k4abt_frame_get_body_id(body_frame, i));
+                    fprintf(fp, "Body ID: %u\n", k4abt_frame_get_body_id(body_frame, i));
+                    printf("\t x \t y \t z \n");
+                    fprintf(fp, "\t x \t y \t z \n");*/
                     for (int j = 0; j < static_cast<int>(K4ABT_JOINT_COUNT); j++)
                     {
                         k4a_float3_t position = skeleton.joints[j].position;
-                        printf("%d:(%.3f, %.3f, %.3f)\n", j, position.v[0], position.v[1], position.v[2]);
+                        printf("%d:(%.3f,%.3f,%.3f)\n", j, position.v[0], position.v[1], position.v[2]);
+                        fprintf(fp, "%d:(%.3f,%.3f,%.3f)\n", j, position.v[0], position.v[1], position.v[2]);
                     }
                 }
             }
         }
     }
+    fclose(fp);
+    printf("Finished Data Convertion!\n");
+    k4a_device_close(device);
 }
